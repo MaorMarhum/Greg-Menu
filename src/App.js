8eae738cd556a85ee94c03765f9a61a9da7bd881
maor marhum <maormarhum9400@gmail.com>
2022-12-25 01:33:54 +0200

    changes4

--- src/App.js
+++ src/App.js
@@ -6,21 +6,27 @@ import items from './data';
 const allCategories = [...new Set(items.map((item) => item.category))]
 
 function App() {
-  const [menuItems, setMenuItems] = useState(items)
+  const initial = items.filter((item) => item.category === 'ארוחות בוקר')
+  const [menuItems, setMenuItems] = useState(initial)
   const [categories, setCategories] = useState(allCategories)
   const [message, setMessage] = useState("")
 
+  // const boker = () => {
+  //   setMessage(`ארוחות הבוקר (זוגי/יחיד) מוגשות עם קפה קטן ושתייה קרה קטנה (תפוזים/גזר/לימונדה). שינוי או הגדלת שתייה כרוכה בתוספת תשלום.`)
+  // }
+
   const filterItems = (category) => {
-    // if (category === 'all') {
-    //   setMenuItems(items)
-    //   return
-    // }
     if(category === 'ארוחות בוקר') {
-      setMessage(`ארוחות הבוקר מוגשות עם קפה קטן ושתייה קרה קטנה (תפוזים/גזר/לימונדה). שינוי או הגדלת שתייה כרוכה בתוספת תשלום.`)
+      setMessage(`ארוחות הבוקר (זוגי/יחיד) מוגשות עם קפה קטן ושתייה קרה קטנה (תפוזים/גזר/לימונדה). שינוי או הגדלת שתייה כרוכה בתוספת תשלום.`)
+    } else if (category === 'סלטים') {
+      setMessage('מוגשים עם לחם הבית, טחינה וסלסת פלפלים.')
+    } else {
+      setMessage("")
     }
     const newItems = items.filter((item) => item.category === category)
     setMenuItems(newItems)
   }
+
 
   if (items.length < 0 && message === 8) {
     setCategories("")
@@ -35,6 +41,9 @@ function App() {
         </div>
         <Categories filterItems={filterItems} categories={categories} />
         <h4>{message}</h4>
+        {
+          menuItems == initial ? <h4>ארוחות הבוקר (זוגי/יחיד) מוגשות עם קפה קטן ושתייה קרה קטנה (תפוזים/גזר/לימונדה). שינוי או הגדלת שתייה כרוכה בתוספת תשלום.</h4> : null
+        }
         <br></br>
         <Menu items={menuItems}/>
       </section>
